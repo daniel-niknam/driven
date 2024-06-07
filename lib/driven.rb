03b7790201ada0a4bddd72f928692315d50b1d34
Daniel Niknam <daniel-niknam@users.noreply.github.com>
2024-06-08 01:28:14 +1000

    Add a simple cli module

--- lib/driven.rb
+++ lib/driven.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "driven/version"
+require_relative "driven/cli"
 
 module Driven
   class Error < StandardError; end
---
+++ lib/driven/cli.rb
@@ -0,0 +1,10 @@
+require "thor"
+
+module Driven
+  class Cli < Thor
+    desc "foo", "Prints foo"
+    def foo
+      puts "foo command"
+    end
+  end
+end
